b179f54db0f31b30bbc3487576030a4c8bf4af50
Chance <chancesmithb@gmail.com>
2024-01-21 06:28:48 -0600

    📦 FEAT: add vite + bun app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Primitive Queue</h1>
+      <h2>actions:</h2>
+      <h2>list:</h2>
+      <ol></ol>
+    </div>
+  );
+}
+
+export default App;
