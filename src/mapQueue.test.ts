3ee8e1be08c41b02fee498f1c5a29f3faf5806d7
Chance Smith <chance.smith@lifeway.com>
2024-01-23 09:13:46 -0600

    📦 FEAT: add next and status

--- src/mapQueue.test.ts
+++ src/mapQueue.test.ts
@@ -1,26 +1,33 @@
 import { Download, createMapQueue } from "./mapQueue";
 
+const download1: Download = {
+  id: "1",
+  videoId: "1",
+  downloadToken: "1",
+};
+const download2: Download = {
+  id: "2",
+  videoId: "2",
+  downloadToken: "2",
+};
+
+const handler = jest.fn().mockResolvedValue(null);
+
 describe("queue", () => {
-  const download1: Download = {
-    id: "1",
-    videoId: "1",
-    downloadToken: "1",
-  };
-  const download2: Download = {
-    id: "2",
-    videoId: "2",
-    downloadToken: "2",
-  };
+  beforeEach(() => {
+    handler.mockClear();
+  });
 
   it("should start empty", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
+    expect(queue.getStatus()).toBe("idle");
     expect(queue.length()).toBe(0);
     expect(queue.head()).toBeUndefined();
     expect(queue.tail()).toBeUndefined();
     expect(queue.isEmpty()).toBe(true);
   });
   it("should have size 1 when one item is added", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     queue.enqueue(download1);
     expect(queue.length()).toBe(1);
     expect(queue.head()).toBe(download1);
@@ -32,7 +39,7 @@ describe("queue", () => {
     expect(queue.tail()).toBe(download2);
   });
   it("should have size 0 when one item is added and removed", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     queue.enqueue(download1);
     queue.enqueue(download2);
 
@@ -47,11 +54,11 @@ describe("queue", () => {
     expect(queue.tail()).toBeUndefined();
   });
   it("should have return undefined if queue is empty", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     expect(queue.dequeue()).toBe(undefined);
   });
   it("should be able to see active item in queue", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     queue.enqueue(download1);
     queue.enqueue(download2);
     expect(queue.peek()).toBe(download1);
@@ -61,25 +68,25 @@ describe("queue", () => {
     expect(queue.peek()).toBe(undefined);
   });
   it("should list items in queue", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     queue.enqueue(download1);
     queue.enqueue(download2);
     expect(queue.list()).toEqual([download1, download2]);
   });
   it("should prevent adding duplicate items", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     expect(queue.enqueue(download1)).toBe(true);
     expect(queue.enqueue(download1)).toBe(false);
   });
   it("should see into any item in queue", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     expect(queue.peekAt(download1.id)).toBeUndefined();
     queue.enqueue(download1);
     queue.enqueue(download2);
     expect(queue.peekAt(download1.id)).toBe(download1);
   });
   it("should drop item from queue by ID", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     expect(queue.drop(download1.id)).toBe(false);
     queue.enqueue(download1);
     expect(queue.length()).toBe(1);
@@ -87,7 +94,7 @@ describe("queue", () => {
     expect(queue.length()).toBe(0);
   });
   it("should clear the queue", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     queue.enqueue(download1);
     queue.enqueue(download2);
     expect(queue.length()).toBe(2);
@@ -95,10 +102,38 @@ describe("queue", () => {
     expect(queue.length()).toBe(0);
   });
   it("should load a queue from a list", () => {
-    const queue = createMapQueue<Download>();
+    const queue = createMapQueue<Download>(handler);
     queue.load([download1, download2]);
     expect(queue.length()).toBe(2);
     expect(queue.head()).toBe(download1);
     expect(queue.tail()).toBe(download2);
   });
+  it("should call handler on next if item in queue", () => {
+    handler.mockImplementation((videoId) => {
+      expect(videoId).toBe(download1.videoId);
+    });
+    const queue = createMapQueue<Download>(handler);
+    expect(handler).toHaveBeenCalledTimes(0);
+    queue.next();
+    expect(handler).toHaveBeenCalledTimes(0);
+
+    queue.enqueue(download1);
+    queue.next();
+    expect(queue.getStatus()).toBe("running");
+    expect(handler).toHaveBeenCalledTimes(1);
+
+    queue.dequeue();
+    expect(queue.getStatus()).toBe("running");
+    expect(queue.isEmpty()).toBeTruthy();
+    queue.next();
+    expect(queue.getStatus()).toBe("idle");
+  });
+  it("should not call handler if queue is empty", () => {
+    const queue = createMapQueue<Download>(handler);
+    queue.next();
+    expect(queue.getStatus()).toBe("idle");
+    expect(handler).not.toHaveBeenCalled();
+  });
 });
+
+// QueueMonitor to always be processing the next item in the queue
--- src/mapQueue.ts
+++ src/mapQueue.ts
@@ -2,12 +2,16 @@ export type Queuable = {
   id: string;
 };
 
+type Handler = (id: string) => Promise<void>;
+type Status = "idle" | "running";
+
 export type Download = {
   videoId: string;
   downloadToken: string;
 } & Queuable;
 
 type Queue<T> = {
+  getStatus: () => Status;
   enqueue: (item: T) => boolean;
   dequeue: () => T | undefined;
   isEmpty: () => boolean;
@@ -20,56 +24,86 @@ type Queue<T> = {
   drop: (id: string) => boolean;
   clear: () => void;
   load: (list: T[]) => void;
+  next: () => void;
 };
 
-export function createMapQueue<T extends Queuable>(): Queue<T> {
+export function createMapQueue<T extends Queuable>(handler: Handler): Queue<T> {
+  let status: Status = "idle";
   const queue: Map<string, T> = new Map();
 
-  return {
-    enqueue: (item: T) => {
-      if (queue.has(item.id)) {
-        return false;
-      }
-      queue.set(item.id, item);
-      return true;
-    },
-    dequeue: () => {
-      const iterator = queue.values();
-      const firstItem = iterator.next().value;
-      if (firstItem) {
-        queue.delete(firstItem.id);
-      }
-      return firstItem;
-    },
-    length: () => queue.size,
-    isEmpty: () => queue.size === 0,
-    peek: () => queue.values().next().value,
-    peekAt: (id: string) => queue.get(id),
-    list: () => Array.from(queue.values()),
-    head: () => queue.values().next().value,
-    tail: () => {
-      const iterator = queue.values();
-      let lastItem: T | undefined = undefined;
-      for (let item of iterator) {
-        lastItem = item;
-      }
-      return lastItem;
-    },
-    drop: (id: string) => {
-      if (queue.has(id)) {
-        queue.delete(id);
-        return true;
-      }
+  const length = () => queue.size;
+  const isEmpty = () => queue.size === 0;
+  const peek = () => queue.values().next().value;
+  const peekAt = (id: string) => queue.get(id);
+  const list = () => Array.from(queue.values());
+  const head = () => queue.values().next().value;
+  const clear = () => queue.clear();
+
+  const enqueue = (item: T) => {
+    if (queue.has(item.id)) {
       return false;
-    },
-    clear: () => queue.clear(),
-    load: (list: T[]) => {
-      list.forEach((item) => queue.set(item.id, item));
-    },
+    }
+    queue.set(item.id, item);
+    return true;
+  };
+
+  const dequeue = () => {
+    const iterator = queue.values();
+    const firstItem = iterator.next().value;
+    if (firstItem) {
+      queue.delete(firstItem.id);
+    }
+    return firstItem;
+  };
+
+  const tail = () => {
+    const iterator = queue.values();
+    let lastItem: T | undefined = undefined;
+    for (let item of iterator) {
+      lastItem = item;
+    }
+    return lastItem;
+  };
+
+  const drop = (id: string) => {
+    if (queue.has(id)) {
+      const wasFirstItem = queue.get(id) === head();
+      queue.delete(id);
+      return true;
+    }
+    return false;
+  };
+
+  const load = (list: T[]) => {
+    list.forEach((item) => queue.set(item.id, item));
+  };
+
+  const next = () => {
+    if (isEmpty()) {
+      status = "idle";
+      return;
+    } else {
+      status = "running";
+      handler(peek().id);
+    }
+  };
+
+  const getStatus = () => status;
+
+  return {
+    enqueue,
+    dequeue,
+    isEmpty,
+    peek,
+    peekAt,
+    length,
+    list,
+    head,
+    tail,
+    drop,
+    clear,
+    load,
+    next,
+    getStatus,
   };
 }
-
-type QueueManager<T> = {
-  state: () => "waiting";
-  queue: () => Queue<T>;
-};
